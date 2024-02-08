f638cef99dd27b0ade68f58be2a619858872d6b2
skykiller30 <74386191+skykiller30@users.noreply.github.com>
2024-02-08 14:55:41 -0600

    Update GetAccessToken(stub).py

--- GetAccessToken(stub).py
+++ GetAccessToken(stub).py
@@ -1,19 +1,22 @@
+# Run this once as part of Initial Set Up, or skip if you did example #1 on EcoBee's developer website and obtained all the secrets that way
+
 import requests
 import json
 secrets_file = "secrets.json"
 
 with open(secrets_file) as f:
     secrets_data = json.load(f)
+    
+authorization_code = secrets_data["AUTHORIZATION_CODE"]
+api_key = secrets_data["API_KEY"]
 
-
-
-def get_access_token(authorization_code, client_id, api_key):
+def get_access_token(authorization_code, api_key):
     url = "https://api.ecobee.com/token"
     payload = {
         "grant_type": "ecobeePin",
         "code": authorization_code,
-        "client_id": client_id,
-        "client_secret": api_key
+        "client_id": api_key,
+        
     }
     response = requests.post(url, data=payload)
     
@@ -23,6 +26,13 @@ def get_access_token(authorization_code, client_id, api_key):
         print("Error:", response.status_code)
         return None
 
+access_token = response["access_token"]
+refresh_token = response["refresh_token"]
+secrets_data["ACCESS_TOKEN"] = access_token
+secrets_date["REFRESH_TOKEN"] = refresh_token
+secrets_data[
+        with open(secrets_file, 'w') as file:
+            json.dump(secrets_data, file, indent=4)
 
 
 
