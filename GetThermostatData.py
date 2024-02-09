bf7d443e4b8eb487edeadbc1f3ed8dfc54cadbe0
skykiller30 <lukeskykiller30@gmail.com>
2024-02-09 06:53:42 +0000

    restructured if statements

--- GetThermostatData.py
+++ GetThermostatData.py
@@ -21,41 +21,41 @@ def get_thermostat_data(access_token):
         "body": '{"selection":{"selectionType":"registered","selectionMatch":""}}'
     }
     response = requests.get(url, headers=headers, params=params)
+    return response
 
-    ecoBeeResponse = response.json()
-    
-    if response.status_code == 200:
-       
-        return ecoBeeResponse
-    elif response.status_code == 500 and "message" in ecoBeeResponse["status"] and "Authentication token has expired. Refresh your tokens." in ecoBeeResponse["status"]["message"]:
-        def getRefreshToken(refresh_token, api_key):
-            url = "https://api.ecobee.com/token"
-            payload = {
-                "grant_type": "refresh_token",
-                "code": refresh_token,
-                "client_id": api_key,
-            }
-            response = requests.post(url, data=payload)
+ecoBeeResponse = get_thermostat_data(access_token)
 
-            if response.status_code == 200:
-                return response.json()
-            else:
-                print("Error:", response.status_code)
-                return None
+if ecoBeeResponse.status_code == 200:
+    ecoBeeResponse=ecoBeeResponse.json()
+    secrets_data.update(ecoBeeResponse)
+    with open(secrets_file, 'w') as file:
+           json.dump(secrets_data, file, indent=4)
+    print("Sucessful! Result:", json.dumps(secrets_data, indent=4))
+
+elif ecoBeeResponse.status_code == 500 and "message" in ecoBeeResponse.json()["status"] and "Authentication token has expired. Refresh your tokens." in ecoBeeResponse.json()["status"]["message"]:
+    def getRefreshToken(refresh_token, api_key):
+        url = "http://api.ecobee.com/token"
+        payload = {
+            "grant_type": "refresh_token",
+            "code": refresh_token,
+            "client_id": api_key,
+        }
+        response = requests.post(url, data=payload)
+
+        if response.status_code == 200:
             
-        token_response = getRefreshToken(refresh_token, api_key)
-        new_access_token = token_response["access_token"]
-        secrets_data["ACCESS_TOKEN"]=new_access_token
-        with open(secrets_file, 'w') as file:
-            json.dump(secrets_data, file, indent=4)
-        return print("The access token has expired, but the secrets.json file has been updated with a new token. Please run again")
+            return response.json()
+        else:
+            print("Error:", response)
+            return None
+    ecoBeeResponse=ecoBeeResponse.json()
+    token_response = getRefreshToken(refresh_token, api_key)
+    new_access_token = token_response["access_token"]
+    secrets_data["ACCESS_TOKEN"]=new_access_token
+    with open(secrets_file, 'w') as file:
+        json.dump(secrets_data, file, indent=4)
+        secrets_data.update(ecoBeeResponse)
+    print("The access token has expired, but the secrets.json file has been updated with a new token. Please run again")
         
-    else:
-        print("Error:", response.status_code)
-        return None
-
-thermostat_data = get_thermostat_data(access_token)
-secrets_data.update(thermostat_data)
-with open(secrets_file, 'w') as file:
-            json.dump(secrets_data, file, indent=4)
-print(json.dumps(secrets_data, indent=4))
+else:
+    print("Error:", ecoBeeResponse.status_code)
