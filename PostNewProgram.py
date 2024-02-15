d229800d5ea924ced559739c46b23724a7cc1a4b
skykiller30 <lukeskykiller30@gmail.com>
2024-02-15 05:01:35 +0000

    edited PostNew program stub

--- PostNewProgram.py
+++ PostNewProgram.py
@@ -25,29 +25,9 @@ def get_thermostat_data(access_token):
     return response.json()
 
 thermostat_list = get_thermostat_data(access_token)
-thermostat_list =thermostat_list.get("thermostatList", [])
-#print("Successfull! Result:", json.dumps(thermostat_list, indent=4))
 
-# Find the thermostat with name "Upstairs" and extract its identifier and program
-upstairs_thermostat = next((thermostat for thermostat in thermostat_list if thermostat.get("name") == "Upstairs"), None)
-upstairs_id = upstairs_thermostat.get("identifier") if upstairs_thermostat else None
-upstairs_program = upstairs_thermostat.get("program", {}) if upstairs_thermostat else {}
-
-# Convert the schedule of the upstairs thermostat to JSON string
-upstairs_schedule = json.dumps(upstairs_program.get("schedule", {}))
-
-# Extract climates of the upstairs thermostat
-upstairs_climates = upstairs_program.get("climates", [])
-
-# Find the thermostat with name "Main Floor" and extract its identifier and program
-main_floor_thermostat = next((thermostat for thermostat in thermostat_list if thermostat.get("name") == "Main Floor"), None)
-main_floor_id = main_floor_thermostat.get("identifier") if main_floor_thermostat else None
-main_floor_program = main_floor_thermostat.get("program", {}) if main_floor_thermostat else {}
-
-# Convert the schedule of the main floor thermostat to JSON string
-main_floor_schedule = json.dumps(main_floor_program.get("schedule", {}))
-
-# Extract climates of the main floor thermostat
-main_floor_climates = main_floor_program.get("climates", [])
-
-print(upstairs_id, json.dumps(upstairs_climates,indent=4))
+#if day high is above 70, apply cooler temperature ranges 
+for thermostat in get_thermostat_data['thermostatList']:
+    # Update coolTemp to 760
+    for climate in thermostat['program']['climates']:
+        climate['coolTemp'] = 760
